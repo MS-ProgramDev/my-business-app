7c9912d5505043c4a63770e04572734fd1ef6581
Maor Sapo <123029828+MS-ProgramDev@users.noreply.github.com>
2025-06-12 22:38:31 +0300

    Final commit before submission

--- backend/db.py
+++ backend/db.py
@@ -1,7 +1,11 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+import os
 
-DATABASE_URL = "postgresql://postgres:postgres@localhost:5432/business_db"
-
+load_dotenv()
+DB_USER = os.getenv("DB_USER")
+DB_PASS = os.getenv("DB_PASS")
+DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASS}@localhost:5432/business_db"
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine)
